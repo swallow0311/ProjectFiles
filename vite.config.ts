1c553b59f892c1a500fb59a3a39756a321d8b795
swallow0311 <774207663@qq.com>
2026-06-05 03:18:54 +0800

    fix:修改vite base路径为/，适配Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import dyadTagger from "@dyad-sh/react-vite-component-tagger";
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 export default defineConfig({
-  base: "/ProjectFiles/",
-  plugins: [react(), dyadTagger()],
-});
+  plugins: [react()],
+  base: '/'
+})
